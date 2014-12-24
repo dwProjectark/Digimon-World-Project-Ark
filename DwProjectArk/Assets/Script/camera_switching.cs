1809422c690ea66411cf3a31baa6cc6fade09196
Redeamed <kdavis928@yahoo.com>
2014-12-23 23:31:19 -0700

    Camera/Movement Improvement
    Code created by Salem.  Improvements to movement and digimon partner
    pathing.  String of cameras for testing various perspectives.

--- DwProjectArk/Assets/Script/camera_switching.cs
+++ DwProjectArk/Assets/Script/camera_switching.cs
@@ -2,18 +2,44 @@
 using System.Collections;
 
 public class camera_switching : MonoBehaviour {
-	public Camera camera_enabled;
-	public Camera camera_disabled;
+	public Camera[] camera;
+
 	// Use this for initialization
 	void Start () {
-	
+		camera [0].enabled = true;
+		for (int i = 1; i < camera.Length; i++) 
+		{
+			camera[i].enabled = false;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.C)) 
+		{
+			for (int i =0; i < camera.Length;i++)
+			{
+				if (camera[i].enabled == true)
+				{
+					camera[i].enabled = false;
+					if (i+1 == camera.Length)
+					{
+						i = 0;
+						camera[i].enabled = true;
+						break;
+					}
+					else
+					{
+						camera[i+1].enabled = true;
+						break;
+					}
+				}
+
+			}
+		}
+
 	}
-	void OnTriggerStay(Collider player)
+	/*void OnTriggerStay(Collider player)
 	{
 		if (player.name == "player") 
 		{
@@ -21,5 +47,5 @@ public class camera_switching : MonoBehaviour {
 			camera_disabled.enabled = false;
 
 		}
-	}
+	}*/
 }
--- DwProjectArk/Assets/Script/player.cs
+++ DwProjectArk/Assets/Script/player.cs
@@ -3,30 +3,43 @@ using System.Collections;
 
 public class player : MonoBehaviour {
 
-	public  float speed = 3;
+	public  float speed = 1;
+
 	bool stat_digimon = false;
-	public Animator anim;
+	Animator animator; 
+	float moving = 4;
+	public Transform body;
+	//Vector3 dircetion = transform.localEulerAngles ;
+	//direction.y = 180.0f;
 
 
-
-
+	
 	// Use this for initialization
 	void Start () {
-		anim.GetComponent<Animator> ();
+		animator = GetComponent<Animator> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		float move_y = Input.GetAxis("Vertical")*Time.deltaTime* speed;
-		float move_x = Input.GetAxis ("Horizontal")*Time.deltaTime*speed;
-		transform.Translate (0, 0, move_y);
-		transform.Translate (move_x, 0, 0);
-		transform.Rotate (0, move_y, 0);
 
-
-			anim.SetFloat("Move",move_y);
+		CharacterController Controller = GetComponent<CharacterController>();
+		Vector3 vertical = transform.TransformDirection (Vector3.forward);
+		Vector3 horizontal = transform.TransformDirection (Vector3.right);
+		animator.SetFloat("Speed",speed);
+		if (Input.GetAxis ("Vertical")>0||Input.GetAxis ("Vertical")<0 
+		    || Input.GetAxis ("Horizontal")>0||Input.GetAxis ("Horizontal")<0) 
+		{
+			animator.SetFloat ("Speed", moving);
+			Controller.Move ((vertical * (speed * Input.GetAxis ("Vertical")) * Time.deltaTime));
+			Controller.Move ((horizontal * (speed * Input.GetAxis ("Horizontal")) * Time.deltaTime));
+		} 
+		else 
+		{
+			animator.SetFloat("Speed",speed);
 		
+		}
 
 		if (Input.GetKeyDown (KeyCode.Escape)) {
 
@@ -47,4 +60,30 @@ public class player : MonoBehaviour {
 			
 
 	}
+	void LateUpdate(){
+		// Rotate the Character to match the direction he/she is going
+		if(Input.GetAxis("Vertical") == 0){
+			if(Input.GetAxis("Horizontal") > 0){
+				transform.localEulerAngles = new Vector3(0f,90f,0f);
+				//transform.localEulerAngles = dircetion;
+			}else if(Input.GetAxis("Horizontal") < 0){
+				transform.localEulerAngles = new Vector3(0f,-90f,0f);
+			}
+		}else if(Input.GetAxis("Vertical") > 0){
+			if(Input.GetAxis("Horizontal") > 0){
+				transform.localEulerAngles = new Vector3(0f,45f,0f);
+			}else if(Input.GetAxis("Horizontal") < 0){
+				transform.localEulerAngles = new Vector3(0f,-45f,0f);
+			}
+		}else if(Input.GetAxis("Vertical") < 0){
+			if(Input.GetAxis("Horizontal") == 0){
+				transform.localEulerAngles = new Vector3(0f,0f,0f);
+			}else if(Input.GetAxis("Horizontal") > 0){
+				transform.localEulerAngles = new Vector3(0f,135f,0f);
+			}else if(Input.GetAxis("Horizontal") < 0){
+				transform.localEulerAngles = new Vector3(0f,-135f,0f);
+			}
+		}
+	}
 }
+
