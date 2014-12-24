16e8e865045818ed95415f6be068c0b10e2156d4
Redeamed <kdavis928@yahoo.com>
2014-12-23 23:51:11 -0700

    Camera management
    Added cameras under cameraManager to organize hierarchy.  Renamed
    cameras.  Modified camera3 to replicator digimon world 1's isometric
    camera angle.

---
+++ DwProjectArk/Assets/Script/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float smoothing = 5f;
+
+    Vector3 offset;
+
+	// Use this for initialization
+	void Start () 
+    {
+        offset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+
+	}
+}
